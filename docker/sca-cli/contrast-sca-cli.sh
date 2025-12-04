6bf2a6b505b1f270b68d5b9a22686c6c20514112
Doug Jeremias <doug.jeremias@contrastsecurity.com>
2025-12-04 13:31:26 -0500

    - fix sca shell script

--- docker/sca-cli/contrast-sca-cli.sh
+++ docker/sca-cli/contrast-sca-cli.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
 echo 'Downloading Contrast SCA CLI Tool'
-curl --location -O https://pkg.contrastsecurity.com/artifactory/cli/v2/latest/linux/contrast
-chmod +x contrast
+curl --location -o /usr/local/bin/contrast https://pkg.contrastsecurity.com/artifactory/cli/v2/latest/linux/contrast
+chmod +x /usr/local/bin/contrast
 
 FULL_REPO_URL="$GITHUB_SERVER_URL/$GITHUB_REPOSITORY"
-REPO_NAME=$(echo "${{ $GITHUB_REPOSITORY }}" | awk -F/ '{print $2}')
+REPO_NAME=$(echo "$GITHUB_REPOSITORY" | awk -F/ '{print $2}')
 
 echo 'Initiating Contrast SCA Fingerprint Command'
 echo contrast fingerprint --api-key $CONTRAST_GITHUB_APP_API_KEY --authorization $CONTRAST_GITHUB_APP_AUTHORIZATION_KEY --organization-id $CONTRAST_GITHUB_APP_ORG_ID --host $CONTRAST_GITHUB_APP_APP_TS_URL --repository-url $FULL_REPO_URL --repository-name $REPO_NAME --external-id $CONTRAST_GITHUB_APP_INSTALLATION_ID --severity CRITICAL --timeout 600 --log
-contrast fingerprint --api-key $CONTRAST_GITHUB_APP_API_KEY --authorization $CONTRAST_GITHUB_APP_AUTHORIZATION --organization-id $CONTRAST_GITHUB_APP_ORG_ID --host $CONTRAST_GITHUB_APP_TS_URL --repository-url $FULL_REPO_URL --repository-name $REPO_NAME --external-id $CONTRAST_GITHUB_APP_INSTALLATION_ID --severity CRITICAL --timeout 600 --log
+/usr/local/bin/contrast fingerprint --api-key $CONTRAST_GITHUB_APP_API_KEY --authorization $CONTRAST_GITHUB_APP_AUTHORIZATION_KEY --organization-id $CONTRAST_GITHUB_APP_ORG_ID --host $CONTRAST_GITHUB_APP_APP_TS_URL --repository-url $FULL_REPO_URL --repository-name $REPO_NAME --external-id $CONTRAST_GITHUB_APP_INSTALLATION_ID --severity CRITICAL --timeout 600 --log
 
 if [ -f fingerPrintInfo.json ]
 then
@@ -25,8 +25,8 @@ then
 			projectGroupId=`jq -r '.['$i'].projectGroupId' fingerPrintInfo.json`
 			echo "$filePath $repositoryId $projectGroupId"
 
-			echo contrast audit --api-key $CONTRAST_GITHUB_APP_API_KEY --authorization $CONTRAST_GITHUB_APP_AUTHORIZATION --organization-id $CONTRAST_GITHUB_APP_ORG_ID --host $CONTRAST_GITHUB_APP_TS_URL --track --repository-id $repositoryId --project-group-id $projectGroupId --file $filePath --severity CRITICAL --timeout 800 --log
-			contrast audit --api-key $CONTRAST_GITHUB_APP_API_KEY --authorization $CONTRAST_GITHUB_APP_AUTHORIZATION --organization-id $CONTRAST_GITHUB_APP_ORG_ID --host $CONTRAST_GITHUB_APP_TS_URL --track --repository-id $repositoryId --project-group-id $projectGroupId --file $filePath --severity CRITICAL --timeout 800 --log
+			echo /usr/local/bin/contrast audit --api-key $CONTRAST_GITHUB_APP_API_KEY --authorization $CONTRAST_GITHUB_APP_AUTHORIZATION_KEY --organization-id $CONTRAST_GITHUB_APP_ORG_ID --host $CONTRAST_GITHUB_APP_TS_URL --track --repository-id $repositoryId --project-group-id $projectGroupId --file $filePath --severity CRITICAL --timeout 800 --log
+			/usr/local/bin/contrast audit --api-key $CONTRAST_GITHUB_APP_API_KEY --authorization $CONTRAST_GITHUB_APP_AUTHORIZATION_KEY --organization-id $CONTRAST_GITHUB_APP_ORG_ID --host $CONTRAST_GITHUB_APP_TS_URL --track --repository-id $repositoryId --project-group-id $projectGroupId --file $filePath --severity CRITICAL --timeout 800 --log
 		done
 else
 	echo "Contrast SCA analysis completed without running the audit command due to no supported manifests found."
